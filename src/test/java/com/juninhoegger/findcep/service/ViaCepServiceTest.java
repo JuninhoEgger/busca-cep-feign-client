91023fd85f3559c46c0e0d14bf3c3f995836483e
Osvaldo Egger Junior <junior.egger@gmail.com>
2022-08-01 13:44:52 -0300

    ajustes finais

--- src/test/java/com/juninhoegger/findcep/service/ViaCepServiceTest.java
+++ src/test/java/com/juninhoegger/findcep/service/ViaCepServiceTest.java
@@ -1,6 +1,5 @@
 package com.juninhoegger.findcep.service;
 
-import com.juninhoegger.findcep.entity.Endereco;
 import com.juninhoegger.findcep.feign.ViaCepFeignClient;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,7 +22,6 @@ class ViaCepServiceTest {
         //GIVEN
         String cep = "94030210";
         //WHEN
-        Endereco endereco = viaCepService.findByCep(cep);
         //THEN
         assertDoesNotThrow(() -> viaCepService.findByCep(cep));
     }
